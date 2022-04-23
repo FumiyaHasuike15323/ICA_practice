36209b71d4bf05e269bd011cdecc33320f4544ac
r-kishimoto <reo.kishimoto@ezweb.ne.jp>
2022-04-23 21:09:05 +0900

    できてないけど一旦part2

--- ICA.m
+++ ICA.m
@@ -4,13 +4,9 @@ clear; close all; clc;
 s2 = audioread('男でかい.wav'); % 信号源2
 x = [s1 s2].'; % 多次元観測信号
 
-T = size(x.',1)/Fs; % 観測時間
-discreteTime = linspace(0, T, T*Fs).'; % 離散時間(ベクトル)
-deltaTime = T/(T*Fs);
-
+time = 10; % 観測時間
 myu = 10; % ステップサイズ
 L = 30; % 反復回数
-
 sigma = 1;
 
 W = eye(2); % 分離行列を単位行列で初期化
@@ -21,27 +17,24 @@ for l = 0:1:L-1 % 自然勾配法によって分離行列Wを推定
         y = W*x;
         fai = y./(sigma*abs(y)); % スコア関数(ラプラス分布)
         R = fai*y.';
-        E = R/T;
-
-    W=W-myu*(E-I)*W;
+        E = R/time;
+        W=W-myu*(E-I)*W;
 
 end
 
-for t = 0:deltaTime:T-deltaTime
-    y = W*x; % 分離信号
-end
+y = W*x; % 分離信号
 
 y = y.'/max(abs(y),[],"all");
 yFemale = y(:, 1); % ステレオ音声の女採取
 yMale = y(:, 2); % ステレオ音声の男採取
 
-fileName1 = 'sefaiaratedSfaieechFemale.wav';
+fileName1 = '分離後の女性声.wav';
 audiowrite(fileName1,yFemale,Fs);
 
-[sefaiarationSignaFemale,Fs] = audioread(fileName1);
+[yFemale,Fs] = audioread(fileName1);
 sound(yFemale,Fs);
 
-fileName2 = 'sefaiaratedSfaieechMale.wav';
+fileName2 = '分離後の男性声.wav';
 audiowrite(fileName2,yMale,Fs);
 
 [yMale,Fs] = audioread(fileName2);
